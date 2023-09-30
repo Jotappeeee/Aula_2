666cf40ea614f309d7bc6359586490456a6b9b63
Jotappeeee <145858432+Jotappeeee@users.noreply.github.com>
2023-09-30 16:22:54 -0300

    Update exercicios 1-4.py

--- exercicios 1-4.py
+++ exercicios 1-4.py
@@ -9,22 +9,22 @@ print('       ')
 # # 2 -Crie duas variáveis para armazenar seu primeiro nome e sobrenome. Em seguida,
 # #concatene-as para formar seu nome completo e exiba o resultado.
 
-# name1 = input('digite seu nome: ')
-# name2 = input('digite seu nome: ')
-# print('Seu nome completo é:',name1, name2 )
-# print('       ')
+name1 = input('digite seu nome: ')
+name2 = input('digite seu nome: ')
+print('Seu nome completo é:',name1, name2 )
+print('       ')
 
 # # 3 -Peça ao usuário para digitar dois números inteiros e armazene-os em variáveis.
 # # Realize a concatenação desses números como strings e exiba o resultado.
 
-# num1 = str(input('Digite um numero: '))
-# num2 = str(input('Digite um numero: '))
-# print('A junção deles é:',num1 + num2)
-# print('       ')
+num1 = str(input('Digite um numero: '))
+num2 = str(input('Digite um numero: '))
+print('A junção deles é:',num1 + num2)
+print('       ')
 
-# # 4 -Crie uma variável para armazenar a palavra "Python". Em seguida, adicione um
-# #número inteiro ao final da palavra usando a concatenação e exiba o resultado.
+# 4 -Crie uma variável para armazenar a palavra "Python". Em seguida, adicione um
+#número inteiro ao final da palavra usando a concatenação e exiba o resultado.
 
-# py = 'Python'
-# num4 = str(input('Digite um numero inteiro: '))
-# print(py + num4)
+py = 'Python'
+num4 = str(input('Digite um numero inteiro: '))
+print(py + num4)
